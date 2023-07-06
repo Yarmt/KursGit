28cef5ad5c2e5c1cc7a562bda09349473c2c8056
Yarmt <shminke.yaroslav@yandex.ru>
2023-07-06 05:48:44 +0400

    git branch

---
+++ src/main/java/com/example/kurs2/Const.java
@@ -0,0 +1,14 @@
+package com.example.kurs2;
+
+public class Const {
+    //если не переопредилить константы под свою таблицу,
+    // то будут ошибки
+    public static final String USER_TABLE = "user";
+    public static final String USER_ID = "iduser";
+    public static final String USER_FIRSTNAME = "firstname";
+    public static final String USER_LASTNAME = "lastname";
+    public static final String USER_PASSWORD = "password";
+    public static final String USER_LOCATION = "location";
+
+
+}
